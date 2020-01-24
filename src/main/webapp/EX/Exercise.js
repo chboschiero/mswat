94e48bcd58f09f24f707d2f8d74b1b3249d1885e
Chiara Boschiero <boschierochiara@gmail.com>
2020-01-24 09:31:14 +0100

    web

---
+++ src/main/webapp/EX/Exercise.js
@@ -0,0 +1,9 @@
+function changeColor(color) {
+	let target = document.getElementById('target');
+	target.style.backgroundColor = color;
+}
+
+function green() {
+	let target = document.getElementById('target');
+	target.value = 'Green';
+}
